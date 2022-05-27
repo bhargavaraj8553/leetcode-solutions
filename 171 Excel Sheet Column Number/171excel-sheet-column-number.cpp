05a27a1a15781a1174b0c11ab25f26ec72a16055
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2022-05-27 04:08:18 +0000

    Code challenge submission for question: 171 Excel Sheet Column Number

---
+++ 171 Excel Sheet Column Number/171excel-sheet-column-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int sum=0;
+        for (char c: columnTitle){
+            sum= sum*26 + (c-'A'+1);
+        }
+        return sum;
+    }
+};
