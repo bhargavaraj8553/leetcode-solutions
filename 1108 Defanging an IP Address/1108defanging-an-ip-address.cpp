55f84e4915c12934cb52d0e826abdddacfffa86f
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2022-06-24 13:51:11 +0000

    Code challenge submission for question: 1108 Defanging an IP Address

---
+++ 1108 Defanging an IP Address/1108defanging-an-ip-address.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        for(auto it:address){
+            if(it=='.'){
+            ans.push_back('[');ans.push_back('.');ans.push_back(']');
+            }
+            else ans.push_back(it);
+    }
+        return ans;
+    }
+        
+};
