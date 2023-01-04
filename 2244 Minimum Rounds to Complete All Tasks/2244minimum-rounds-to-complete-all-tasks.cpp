762b4e62ff720417efac7e5ea1f2654b490a3ad5
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2023-01-04 17:14:51 +0000

    Code challenge submission for question: 2244 Minimum Rounds to Complete All Tasks

---
+++ 2244 Minimum Rounds to Complete All Tasks/2244minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int> mp;
+        int count=0;
+        for(auto it:tasks){
+            mp[it]++;
+        }
+        for(auto it:mp){
+            if(it.second==1)return -1;
+            if(it.second%3==0)count+= (it.second)/3;
+            else count+= (it.second)/3 + 1;
+        }
+        return count;
+    }
+};
