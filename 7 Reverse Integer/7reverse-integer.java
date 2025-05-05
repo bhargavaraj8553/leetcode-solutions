83d0e68e4c40b669c7dae1fe2453944a16064d7c
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2025-05-05 02:29:54 +0000

    Code challenge submission for question: 7 Reverse Integer

---
+++ 7 Reverse Integer/7reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int result = 0;
+        boolean nagativeFlag = x<0;
+        if (nagativeFlag) x=-x;
+        while(x>0){
+            if(result>Integer.MAX_VALUE/10 || result<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            int l= x%10;
+            result = result*10 + l;
+            x/=10;
+        }
+        return nagativeFlag? -result:result;
+    }
+}
