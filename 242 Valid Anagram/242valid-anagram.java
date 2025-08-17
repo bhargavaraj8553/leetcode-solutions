356a7623d4d2583ecc5e25ec477088820228bc6a
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2025-08-17 22:02:06 +0000

    Code challenge submission for question: 242 Valid Anagram

---
+++ 242 Valid Anagram/242valid-anagram.java
@@ -0,0 +1,13 @@
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] charArrayS = s.toCharArray();
+        char[] charArrayT = t.toCharArray();
+        Arrays.sort(charArrayS);
+        Arrays.sort(charArrayT);
+        return Arrays.equals(charArrayS, charArrayT);
+    }
+}
