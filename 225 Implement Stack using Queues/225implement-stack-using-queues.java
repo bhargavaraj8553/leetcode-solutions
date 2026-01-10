9042dbc86d1005af6fcd41a5b020b68304b3b8a1
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2026-01-10 02:36:32 +0000

    Code challenge submission for question: 225 Implement Stack using Queues

---
+++ 225 Implement Stack using Queues/225implement-stack-using-queues.java
@@ -0,0 +1,43 @@
+class MyStack {
+    Queue<Integer> queue = new LinkedList<>();
+
+
+    public MyStack() {
+        
+    }
+    
+    public void push(int x) {
+        int size = queue.size();
+        queue.add(x);
+        for (int i =0;i<size;i++){
+            queue.add(queue.remove());
+        }   
+    }
+    
+    public int pop() {
+        if (queue.isEmpty()) {
+            throw new RuntimeException("Stack Underflow");
+        }
+        return queue.remove();
+    }
+    
+    public int top() {
+         if (queue.isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        return queue.peek();
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
