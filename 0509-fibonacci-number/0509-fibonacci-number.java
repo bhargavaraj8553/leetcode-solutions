5a241d1508b7d7e4e7d99f34264b0d464de42c95
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2026-04-19 12:17:04 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,29 @@
+
+/**
+ * memoization solution
+ */
 class Solution {
     public int fib(int n) {
-        if (n<=1) return n;
-        return fib(n-1)+ fib(n-2);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fibonacciSeries(n, dp);
+    }
+
+    private int fibonacciSeries(int n, int[] dp) {
+        if (n<=1){
+            return dp[n] = n;
+        }
+        if (dp[n]!=-1) return dp[n];
+        return dp[n] = fibonacciSeries(n-1,dp) + fibonacciSeries(n-2, dp);
     }
 }
+
+/**
+ * basic recursion solution
+ * class Solution {
+ *     public int fib(int n) {
+ *         if (n<=1) return n;
+ *         return fib(n-1)+ fib(n-2);
+ *     }
+ * }
+ */
