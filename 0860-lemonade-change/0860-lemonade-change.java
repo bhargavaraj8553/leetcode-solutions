c9d680235d7b8ac14c1b01e7f370b85a5824a3ff
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2026-06-15 02:11:03 +0530

    Time: 2 ms (99.3%), Space: 72.5 MB (28.32%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,31 +1,24 @@
-class Change{
-    int five;
-    int ten;
-    int twenty;
-}
-
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        Change change= new Change();
+        int five=0;
+        int ten=0;
         for (int bill:bills){
             if (bill==5){
-                change.five++;
+                five++;
             } else if (bill==10){
-                if (change.five>=1){
-                    change.five--;
-                    change.ten++;
+                if (five>=1){
+                    five--;
+                    ten++;
                 }
                 else {
                     return false;
                 }
             } else if (bill==20){
-                if (change.five>=1 && change.ten>=1){
-                    change.ten-=1;
-                    change.five-=1;
-                    change.twenty+=1;
-                } else if (change.five>=3){
-                    change.five-=3;
-                    change.twenty+=1;
+                if (five>=1 && ten>=1){
+                    ten-=1;
+                    five-=1;
+                } else if (five>=3){
+                    five-=3;
                 } else {
                     return false;
                 }
