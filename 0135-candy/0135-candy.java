eb388fac476c16c86fd4c8b4b8c2d9582f464901
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2026-06-29 01:37:37 +0530

    Time: 3 ms (88.99%), Space: 47.7 MB (32.04%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,64 +1,106 @@
-
 class Solution {
     public int candy(int[] ratings) {
-        int sum=1;
-        int i=1;
-        int n= ratings.length;
-        while (i<n){
-            if (ratings[i]==ratings[i-1]){
-                sum+=1;
+
+        // Single Pass Greedy
+        // Treat ratings as mountains (uphill & downhill).
+        // Count increasing slope (peak) and decreasing slope.
+        // If downhill is longer than uphill, compensate the peak.
+        //
+        // TC: O(N)
+        // SC: O(1)
+
+        int sum = 1;
+        int i = 1;
+        int n = ratings.length;
+
+        while (i < n) {
+
+            // Equal ratings -> both can have one candy
+            if (ratings[i] == ratings[i - 1]) {
+                sum += 1;
                 i++;
                 continue;
             }
-            int peak=1;
-            while (i<n && ratings[i]>ratings[i-1]){
-                peak+=1;
-                sum+=peak;
+
+            // Traverse increasing slope
+            int peak = 1;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                peak++;
+                sum += peak;
                 i++;
             }
 
-            int down=1;
-            while (i<n && ratings[i]<ratings[i-1]){
-                sum+=down;
-                down+=1;
+            // Traverse decreasing slope
+            int down = 1;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                sum += down;
+                down++;
                 i++;
             }
-            if (down>peak){
-                sum+= down-peak;
+
+            // Peak should get maximum candies from both slopes
+            if (down > peak) {
+                sum += down - peak;
             }
         }
+
         return sum;
     }
 }
 
+
 //
 //class Solution {
 //    public int candy(int[] ratings) {
+//
+//        // Two Pass Greedy
+//        // 1. Left to Right: Satisfy left neighbour condition.
+//        // 2. Right to Left: Satisfy right neighbour condition.
+//        // 3. Take maximum candies required from both directions.
+//        //
+//        // TC: O(2N)
+//        // SC: O(N)
+//
 //        int n = ratings.length;
 //        int[] left = new int[n];
 //
-//        left[0]=1;
+//        // First child gets one candy
+//        left[0] = 1;
 //
-//        for (int i=1;i<n;i++){
-//            if (ratings[i]>ratings[i-1]){
-//                left[i]= left[i-1]+1;
+//        // Left to Right traversal
+//        for (int i = 1; i < n; i++) {
+//
+//            // Higher rating than left neighbour
+//            if (ratings[i] > ratings[i - 1]) {
+//                left[i] = left[i - 1] + 1;
 //            } else {
-//                left[i]=1;
+//                left[i] = 1;
 //            }
 //        }
-//        int right=1;
-//        int current =1;
-//        int sum=Math.max(1, left[n-1]);
-//        for (int i = n-2;i>=0;i--){
-//            if (ratings[i]>ratings[i+1]){
-//                current= right+1;
+//
+//        // Right traversal starts with one candy
+//        int right = 1;
+//        int current = 1;
+//
+//        // Last child's contribution
+//        int sum = Math.max(1, left[n - 1]);
+//
+//        // Right to Left traversal
+//        for (int i = n - 2; i >= 0; i--) {
+//
+//            // Higher rating than right neighbour
+//            if (ratings[i] > ratings[i + 1]) {
+//                current = right + 1;
 //                right = current;
-//            } else{
-//                current=1;
-//                right=1;
+//            } else {
+//                current = 1;
+//                right = 1;
 //            }
-//            sum+= Math.max(left[i], current);
+//
+//            // Take maximum candies needed from both directions
+//            sum += Math.max(left[i], current);
 //        }
+//
 //        return sum;
 //    }
 //}
