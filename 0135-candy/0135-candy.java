2e056ecbe4020afcc18eca9edda0e1c3230bc054
Rajshekhar Bhargava <Bhargava.rajshekharb@gmail.com>
2026-06-29 01:11:13 +0530

    Time: 3 ms (88.99%), Space: 47.8 MB (32.04%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] left = new int[n];
+
+        left[0]=1;
+
+        for (int i=1;i<n;i++){
+            if (ratings[i]>ratings[i-1]){
+                left[i]= left[i-1]+1;
+            } else {
+                left[i]=1;
+            }
+        }
+        int right=1;
+        int current =1;
+        int sum=Math.max(1, left[n-1]);
+        for (int i = n-2;i>=0;i--){
+            if (ratings[i]>ratings[i+1]){
+                current= right+1;
+                right = current;
+            } else{
+                current=1;
+                right=1;
+            }
+            sum+= Math.max(left[i], current);
+        }
+        return sum;
+    }
+}
